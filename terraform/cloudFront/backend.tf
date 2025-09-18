7262298809e52de89ba3ca79780158fa9a08077f
Navish Jindal <159858741+navishjindal@users.noreply.github.com>
2025-09-18 17:48:12 -0400

    Update backend.tf

--- terraform/cloudFront/backend.tf
+++ terraform/cloudFront/backend.tf
@@ -1,10 +1,10 @@
 terraform { 
   cloud { 
     
-    organization = "sanskrit-gameshow" 
+    organization = "Gameshow" 
 
     workspaces { 
-      name = "gameshow-cloudFront" 
+      name = "Gameshow_CloudFront" 
     } 
   } 
 }
