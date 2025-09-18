c1e68c3bc180a4857bda957eddccbd5e34824138
Navish Jindal <159858741+navishjindal@users.noreply.github.com>
2025-09-18 17:49:22 -0400

    Update backend.tf

--- terraform/s3/backend.tf
+++ terraform/s3/backend.tf
@@ -1,10 +1,10 @@
 terraform { 
   cloud { 
     
-    organization = "sanskrit-gameshow" 
+    organization = "Gameshow" 
 
     workspaces { 
-      name = "gameshow-s3" 
+      name = "Gameshow_S3" 
     } 
   } 
 }
